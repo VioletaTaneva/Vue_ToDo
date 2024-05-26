72875e51ea4ec98d61db6b067b3e6ff00098d0c4
VioletaTaneva <violeta.akaunt11@gmail.com>
2024-05-26 22:48:19 +0200

    First commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <AppButton @btn-click="$emit('toggle-add-task')"
+         :text="showAddTask ? 'Close' : 'Add Task'"
+         :color="showAddTask ? 'red' : 'green'" />
+    </header>
+</template>
+
+<script>
+import AppButton from './AppButton.vue';
+
+export default {
+    name: 'AppHeader',
+    props: {
+        title: String,
+        showAddTask: Boolean,
+    },
+    components: {
+        AppButton
+    }
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 30px;
+}
+</style> 
---
+++ src/components/TasksToDo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div :key="task.id" v-for="task in tasks">
+        <IndividualTask @toggle-reminder="$emit('toggle-reminder', task.id)" @delete-task="$emit('delete-task', task.id)" :task="task" />
+    </div>
+</template>
+
+<script>
+import IndividualTask from './IndividualTask.vue';
+
+export default {
+    name: 'TasksToDo',
+    props: {
+        tasks: Array
+    },
+    components: {
+        IndividualTask
+    },
+    emits: ['delete-task', 'toggle-reminder'],
+}
+</script>
